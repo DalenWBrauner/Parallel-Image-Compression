591e98825ff173fed29173ab9d9f9bdf2859b359
Dalen W Brauner <DalenWBrauner@gmail.com>
2014-01-12 21:13:00 -0500

    Additional DCT test confirms algorithm accurate!
    I do in fact believe there is a 90% chance the textbook has simply
    printed an honest inaccuracy.

--- Compression.py
+++ Compression.py
@@ -33,7 +33,20 @@ def main():
     for mtrx in DCT(UsersImage):
         print mtrx
 
-def Compress(i):
+    UsersImage2 = array([
+    [52, 55, 61,  66,  70,  61, 64, 73],
+    [63, 59, 55,  90, 109,  85, 69, 72],
+    [62, 59, 68, 113, 144, 104, 66, 73],
+    [63, 58, 71, 122, 154, 106, 70, 69],
+    [67, 61, 68, 104, 126,  88, 68, 70],
+    [79, 65, 60,  70,  77,  68, 58, 75],
+    [85, 71, 64,  59,  55,  61, 65, 83],
+    [87, 79, 69,  68,  65,  76, 78, 94]
+    ])
+    for mtrx in DCT(UsersImage2):
+        print mtrx
+
+def Compress(i,):
     """Returns a compressed version of the image."""
     Colors = Seperate_Color_Data(i)
     print "R"
